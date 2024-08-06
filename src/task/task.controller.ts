4c62019e487b21d7a3f6afa71ed97322ea2aa7bb
elshadayzb <elshadayzbruk2019@gmail.com>
2024-08-06 17:23:53 +0300

    Added a dto object

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('task')
 export class TaskController {
@@ -8,5 +9,22 @@ export class TaskController {
   @Get()
   getAllTasks() {
     return this.taskService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id) {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto) {
+    console.log('Task: ', createTaskDto);
+
+    return this.taskService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id) {
+    this.taskService.deleteTask(id);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,10 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TaskService {
-  private tasks = ['Task 1', 'Task 2', 'Task 3'];
+  private tasks: Task[] = [];
 
   getAllTasks() {
     return this.tasks;
+  }
+
+  getTaskById(id: string) {
+    return this.tasks.filter((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto) {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.TODO,
+    };
+
+    console.log('New task created: ', task);
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  deleteTask(id: string) {
+    this.tasks.filter((task) => task.id !== id);
   }
 }
